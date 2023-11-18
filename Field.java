17c1913a6b4f268ad0138444713811420cdb6121
jonaswoetmann <144006671+jonaswoetmann@users.noreply.github.com>
2023-11-18 16:43:45 +0100

    initial ChanceCardDeck

--- Field.java
+++ Field.java
@@ -50,9 +50,84 @@ class Player {
                 '}';
     }
 
+    private int[] cards = new int[20]
+
     public void pickChance() {
         System.out.println("Player: " + player + " pickup chance.");
-        // TODO: player pickup chance
+        
+        for (int i = 0; i < cards.length; i++) {
+            cards[i] = i + 1;
+        }
+
+        int temp = cards[0];
+
+        if (temp == 0) {
+            System.out.println("Ryk frem til Start og modtag M2.")
+        }
+        else if (temp == 1) {
+            System.out.println("Ryk 5 felter frem.")
+        }
+        else if (temp == 2) {
+            System.out.println("Ryk 1 felt frem, eller tag et chancekort mere")
+        }
+        else if (temp == 3) {
+            System.out.println("Ryk frem til Strandpromenaden")
+        }
+        else if (temp == 4) {
+            System.out.println("Du har spist for meget slik. Betal M2 til banken.")
+        }
+        else if (temp == 5) {
+            System.out.println("Det er din fødselsdag! Alle giver dig M1. Tillykke med fødselsdagen!")
+        }
+        else if (temp == 6) {
+            System.out.println("Du har lavet alle dine lektier. Modtag M2 fra banken.")
+        }
+        else if (temp == 7) {
+            System.out.println("Gratis felt! Ryk frem til Skaterparken for at lave det perfekte grind! hvis ingen ejer den, får du den gratis! Ellers skal du betale leje til ejeren.")
+        }
+        else if (temp == 8) {
+            System.out.println("Gratis felt! Ryk frem til et orange felt. Hvis det er ledigt, får du det gratis! Ellers skal du betale leje til ejeren.")
+        }
+        else if (temp == 9) {
+            System.out.println("Gratis felt! Ryk frem til et orange eller grønt felt. Hvis det er ledigt, får du det gratis! Ellers skal du betale leje til ejeren.")
+        }
+        else if (temp == 10) {
+            System.out.println("Gratis felt! Ryk frem til et lyseblåt felt. Hvis det er ledigt, får du det gratis! Ellers skal du betale leje til ejeren.")
+        }
+        else if (temp == 11) {
+            System.out.println("Gratis felt! Ryk frem til et pink eller mørkeblåt felt. Hvis det er ledigt, får du det gratis! Ellers skal du betale leje til ejeren.")
+        }
+        else if (temp == 12) {
+            System.out.println("Gratis felt! Ryk frem til et rødt felt. Hvis det er ledigt, får du det gratis! Ellers skal du betale leje til ejeren.")
+        }
+        else if (temp == 13) {
+            System.out.println("Gratis felt! Ryk frem til et lyseblåt eller rødt felt. Hvis det er ledigt, får du det gratis! Ellers skal du betale leje til ejeren.")
+        }
+        else if (temp == 14) {
+            System.out.println("Gratis felt! Ryk frem til et brunt eller gult felt. Hvis det er ledigt, får du det gratis! Ellers skal du betale leje til ejeren.")
+        }
+        else if (temp == 15) {
+            System.out.println("Du løslades uden omkostninger. Behold dette kort, indtil du får brug for det.")
+        }
+        else if (temp == 16) {
+            System.out.println("Giv dette kort til Bilen, og tag et chancekort mere. Bil: på din næste tur skal du drøne frem til et hvilket som helst ledigt felt og købe det. Hvis der ikke er nogle ledige felter, skal du købe et fra en anden spiller!")
+        }
+        else if (temp == 17) {
+            System.out.println("Giv dette kort til Skibet, og tag et chancekort mere. Skib: på din næste tur skal du selje frem til et hvilket som helst ledigt felt og købe det. Hvis der ikke er nogle ledige felter, skal du købe et fra en anden spiller!")
+        }
+        else if (temp == 18) {
+            System.out.println("Giv dette kort til Katten, og tag et chancekort mere. Kat: på din næste tur skal du liste dig hen på et hvilket som helst ledigt felt og købe det. Hvis der ikke er nogle ledige felter, skal du købe et fra en anden spiller!")
+        }
+        else if (temp == 19) {
+            System.out.println("Giv dette kort til Hunden, og tag et chancekort mere. Hund: på din næste tur skal du hoppe hen på et til et hvilket som helst ledigt felt og købe det. Hvis der ikke er nogle ledige felter, skal du købe et fra en anden spiller!")
+        }
+
+        for (int i = 0; i < cards.length - 1; i++) {
+            cards[i] = cards[i + 1];
+        }
+
+        cards[cards.length - 1] = temp;
+        
     }
 }
 
