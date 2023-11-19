dd3be0bfb6510ac3aa6cd83538288838a325fd61
anton dong <miax6351@gmil.com>
2023-11-19 22:09:01 +0100

    correct debug message

--- monopoly/src/main/java/monopoly/JailField.java
+++ monopoly/src/main/java/monopoly/JailField.java
@@ -2,14 +2,15 @@ package monopoly;
 
 public class JailField extends Field implements FieldAction {
     public JailField(String name, int position) {
-        super(name, position);
+        super(name);
+        this.position = position;
     }
 
     public void doAction(Player player) {
         if(player.getInJail()) {
             // TODO : handle this
         } else {
-             System.out.println("Player: " + player + " visit jail.");
+             System.out.println("Player: " + player.getPlayerName() + " visit jail.");
         }
     }
 }
